5a30d152fc6d5119f285301e2285b2c59a951393
David Witherspoon <david.witherspoon@kaazing.com>
2017-06-09 10:43:26 -0700

    Updated script for ws to tcp proxy

--- src/main/scripts/ws-tcp-proxy.js
+++ src/main/scripts/ws-tcp-proxy.js
@@ -24,15 +24,15 @@ headers.put(":authority", "localhost:8080");
 var port = 8080;
 var address = InetAddress.getByName("127.0.0.1");
 
-tcpController.routeOutputNew("ws", 0, "127.0.0.1", 1050, null)
+tcpController.routeClient("ws", 0, "127.0.0.1", 1050, null)
     .thenCompose(function (tcpOutputRef) {
-        return wsController.routeInputNew("http", 0, "tcp", tcpOutputRef, null);
+        return wsController.routeServer("http", 0, "tcp", tcpOutputRef, null);
     })
     .thenCompose(function (wsInputRef) {
-        return httpController.routeInputNew("tcp", 0, "ws", wsInputRef, headers);
+        return httpController.routeServer("tcp", 0, "ws", wsInputRef, headers);
     })
     .thenCompose(function (httpInputRef) {
-        return tcpController.routeInputNew("any", port, "http", httpInputRef, address);
+        return tcpController.routeServer("any", port, "http", httpInputRef, address);
     })
     .thenAccept(function () {
         print("WS proxy bound to localhost:8080");
